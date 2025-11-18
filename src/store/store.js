bf608adf56f52f9c6bf007b77a2f664e494757d1
AtharvG13 <atharvgadhve392@gmail.com>
2025-11-18 22:45:03 +0530

    Removed extra files, kept only clientside and serverside

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./slice/User/user.slice.js";
-
-export const store = configureStore({
-  reducer: {
-    userReducer,
-  },
-});
